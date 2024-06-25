16967e36307c24bcc655879783c4aee2c0d0f467
Janvii1505 <janvii1505@gmail.com>
2024-06-25 23:53:35 +0530

    Delete Module_3 directory

--- Module_3/Basic_logic_programme/15_school_abbr.c
+++
@@ -1,9 +0,0 @@
-//15.Convert school’s name in abbreviated form
-#include<stdio.h>
-main(){
-	char school[20] = "Ryan International School";
-
-	printf("\n\n\t School : %c.%c.%c",school[0],school[5],school[19]);
-	
-	
-}
--- Module_3/Basic_logic_programme/16_country_abbr.c
+++
@@ -1,9 +0,0 @@
-//16.Convert country’s name in abbreviate form
-#include<stdio.h>
-main(){
-	char country[30] = "United States of America";
-
-	printf("\n\n\t Country : %c.%c.%c.",country[0],country[7],country[17]);
-	
-	
-}
