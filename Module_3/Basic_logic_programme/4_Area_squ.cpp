7080b0b7921f27c4179fab7dab3abaa49d0ccd88
Janvii1505 <janvii1505@gmail.com>
2024-06-25 23:36:23 +0530

    C_language

---
+++ Module_3/Basic_logic_programme/4_Area_squ.cpp
@@ -0,0 +1,11 @@
+//4. Find Area of Square formula : a = a2
+#include<stdio.h>
+main(){
+	int a;
+	printf("\n\n\t Enter any number : ");
+	scanf("%d",&a);
+	
+	printf("\n\n\t Area of a Square is : %d",a*a);
+	
+	
+}
